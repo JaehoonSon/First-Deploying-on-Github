c957fe01da60f3adcda4f87f241a1cfef53c9c18
JaehoonSon <65527375+JaehoonSon@users.noreply.github.com>
2020-08-21 17:06:04 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-$txt = "";
-function myFunction() {
-    if(confirm("OKAY?")){
-        $txt = "You are OKAY!"
-    }else{
-        $txt = "You are NOT OKAY!"
-    }
-    document.getElementById("demo").innerHTML = $txt;
-   }
+let x=1;
